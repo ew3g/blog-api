5d0c4b903ab1e8937b3f3cce72e541a7e8d48b46
Edilson Rodrigues <edilson.w3g@gmail.com>
2022-10-21 21:35:49 -0300

    chore: first commit

---
+++ posts/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from .models import Post
+
+
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user
+        test_user_1 = User.objects.create_user(username='test_user_1', password='abc123')
+        test_user_1.save()
+
+        # Create a blog post
+        test_post = Post.objects.create(author=test_user_1, title='Blog title', body='body content')
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'test_user_1')
+        self.assertEqual(title, 'Blog title')
+        self.assertEqual(body, 'body content')
+
+
